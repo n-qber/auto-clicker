bed3fe06178afed4fb873a4614b1482aeaab5615
n-qber <45954188+n-qber@users.noreply.github.com>
2021-01-09 00:38:05 -0300

    I did it before opening this repository so skjdfhs

---
+++ run.py
@@ -0,0 +1,48 @@
+from pynput.mouse import Controller, Button
+from pynput.keyboard import Listener
+from threading import Thread
+from time import sleep
+
+TPC = .05
+CLICK = False
+mouse = Controller()
+
+def start_clicking():
+    #print("[*] Start clicking")
+    global CLICK
+    CLICK = True
+    while CLICK:
+        mouse.click(Button.left, 1)
+        sleep(TPC)
+
+def stop_clicking():
+    #print("[*] Stop clicking")
+    global CLICK
+    CLICK = False
+
+def toggle_clicking():
+    #print("[*] Toggle clicking")
+    global CLICK
+    CLICK = not CLICK
+    if CLICK:
+        Thread(target=start_clicking).start()
+
+press = {"c": lambda: Thread(target=start_clicking).start(),
+         "t": lambda: Thread(target=toggle_clicking).start()}
+
+release = {"c": lambda: Thread(target=stop_clicking).start()}
+
+def on_press(key):
+    if hasattr(key, "char"):
+        press.get(key.char, lambda:None)()
+
+def on_release(key):
+    if hasattr(key, "char"):
+        release.get(key.char, lambda: None)()
+
+print("======= AUTO CLICKER 1.0 =======")
+print("[*] Author: Nicolas Bertozzo\n\n")
+print("[*] Press C: start clicking\n[*] Release C: stop clicking\n[*] Press T: toggle clicking")
+
+with Listener(on_press=on_press, on_release=on_release) as l:
+    l.join()
